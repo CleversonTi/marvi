cd12c9aa27c77502c32129f3281fd39e63087206
CleversonTi <clevinho_tiago@hotmail.com>
2025-04-04 16:41:19 -0300

    Ajuste no index

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,11 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import MainRoutes from './MainRoutes';
 import AuthRoutes from './AuthRoutes';
+import { useAuthStore } from '@/stores/useAuthStore';
 
-export default createRouter({
+const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
-        ...MainRoutes, // Importando corretamente o array de rotas
+        ...MainRoutes,
         ...AuthRoutes,
         {
             path: '/:pathMatch(.*)*',
@@ -13,3 +14,17 @@ export default createRouter({
         }
     ]
 });
+
+// Proteção de rotas que exigem autenticação
+router.beforeEach((to, from, next) => {
+    const authStore = useAuthStore();
+    authStore.carregarToken();
+
+    if (to.meta.requiresAuth && !authStore.token) {
+        next('/login');
+    } else {
+        next();
+    }
+});
+
+export default router;
